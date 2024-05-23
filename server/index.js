944df7fe6552dca983e54193427163471d280771
parthkathiriya90 <parth@sourcenettechnology.in>
2024-05-23 12:52:58 +0530

    add env file.

--- server/index.js
+++ server/index.js
@@ -1,29 +1,29 @@
 // server/server.js
-const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
-const { createProxyMiddleware } = require('http-proxy-middleware');
-const path = require('path');
-const socketConfig = require('./config/socket');
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+const path = require("path");
+const socketConfig = require("./config/socket");
+require('dotenv').config();
+
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: '*',
-    methods: ['GET', 'POST'],
+    origin: "*",
+    methods: ["GET", "POST"],
   },
 });
 
-// Proxy requests to the live URL of the React app
-const CLIENT_URL = 'http://localhost:3000';
 app.use(
-  '/',
+  "/",
   createProxyMiddleware({
-    target: CLIENT_URL,
+    target: process.env.CLIENT_SOCKET_URL,
     changeOrigin: true,
     pathRewrite: {
-      '^/': '/', // remove base path
+      "^/": "/", // remove base path
     },
   })
 );
